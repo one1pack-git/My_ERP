e6178c7a68b66a3fc75115a59e9f3a09ecf80aaa
one1pack-git <one1pack2015@gmail.com>
2025-03-02 20:50:26 +0900

    Remove build/ directory from Git tracking

--- build/.qt/QtDeployTargets-Release.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_QtSQLiteTest_FILE C:/Qt/Project/My_ERP/build/Release/QtSQLiteTest.exe)
-set(__QT_DEPLOY_TARGET_QtSQLiteTest_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_QtSQLiteTest_RUNTIME_DLLS C:/Qt/6.8.2/msvc2022_64/bin/Qt6Sql.dll;C:/Qt/6.8.2/msvc2022_64/bin/Qt6Core.dll)
