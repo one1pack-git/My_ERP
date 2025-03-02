8dce8c7ac03eb57fcfa578a63e1b5810e2d57383
one1pack-git <one1pack2015@gmail.com>
2025-03-02 20:47:41 +0900

    test

---
+++ build/.qt/QtDeployTargets-Debug.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_QtSQLiteTest_FILE C:/Qt/Project/My_ERP/build/Debug/QtSQLiteTest.exe)
+set(__QT_DEPLOY_TARGET_QtSQLiteTest_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_QtSQLiteTest_RUNTIME_DLLS C:/Qt/6.8.2/msvc2022_64/bin/Qt6Sqld.dll;C:/Qt/6.8.2/msvc2022_64/bin/Qt6Cored.dll)
